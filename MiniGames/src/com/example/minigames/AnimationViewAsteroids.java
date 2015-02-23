e0c66295578758c4121dfe606d9623971d62e62a
unknown <WilliamTrainor16@wtrainor16.MaristSchool.Local>
2015-02-23 15:35:46 -0500

    added 2/23/15's work

---
+++ MiniGames/src/com/example/minigames/AnimationViewAsteroids.java
@@ -0,0 +1,18 @@
+package com.example.minigames;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class AnimationViewAsteroids extends View {
+
+	
+	
+	public AnimationViewAsteroids(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+	} // end constructor
+
+	
+	
+} // end AnimationViewAsteroids
--- MiniGames/src/com/example/minigames/AnimationViewConnect4.java
+++ MiniGames/src/com/example/minigames/AnimationViewConnect4.java
@@ -2,13 +2,25 @@ package com.example.minigames;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class AnimationViewConnect4 extends View {
 
+	public int[][] board = new int[7][6];
+	
 	public AnimationViewConnect4(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
+		
 	} // end onCreate
 
+	public boolean onTouchEvent(MotionEvent event){ 
+		int action = event.getActionMasked(); 
+		int actionIdex = event.getActionIndex(); 
+		
+		//paddle.goTo((int)event.getX(), 750);
+		
+	return true;
+	} // end onTouchEvnet 
+	
 } // end class AnimationViewConnect4
